826eb753c9067ce942b28762e9105a4153e4ab14
truls <trulsmoholt@hotmail.com>
2021-02-04 16:59:33 +0100

    regner ut normaler og midtpunkter først

--- FVMO.py
+++ FVMO.py
@@ -42,30 +42,34 @@ def compute_matrix(mesh,K):
             #D
             v = nodes[i,j-1]-nodes[i,j]
             interface[3,:] = nodes[i,j] + 0.5*(v)
-            n[3,:] = 0.5*np.array([v[1],-v[0]])
+            n[3,:] = 0.5*np.array([-v[1],v[0]])
+            centers[3,:] = cell_centers[i,j-1]
+
             #A
             v = nodes[i-1,j]-nodes[i,j]
             interface[0,:] = nodes[i,j] + 0.5*(v)
             n[0,:] = 0.5*np.array([-v[1],v[0]])
+            centers[0,:] = cell_centers[i-1,j-1]
             #B
             v = nodes[i,j+1]-nodes[i,j]
             interface[1,:] = nodes[i,j] + 0.5*(v)
             n[1,:] = 0.5*np.array([-v[1],v[0]])
+            centers[1,:] = cell_centers[i-1,j]
+
             #C
             v = nodes[i+1,j]-nodes[i,j]
             interface[2,:] = nodes[i,j] + 0.5*(v)
-            n[2,:] = 0.5*np.array([v[1],-v[0]])
+            n[2,:] = 0.5*np.array([-v[1],v[0]])
+            centers[2,:] = cell_centers[i,j]
 
-            centers[0,:] = cell_centers[i-1,j-1]
+
+
             k_loc[0] = k_global[i-1,j-1]
 
-            centers[1,:] = cell_centers[i-1,j]
             k_loc[1] = k_global[i-1,j]
 
-            centers[2,:] = cell_centers[i,j]
             k_loc[2] = k_global[i,j]
 
-            centers[3,:] = cell_centers[i,j-1]
             k_loc[3] = k_global[i,j-1]
 
             V = np.zeros((4,2,2))
@@ -81,14 +85,14 @@ def compute_matrix(mesh,K):
             for ii in range(4):
                 for jj in range(4):
                     for kk in range(2):
-                        omega[ii,jj,kk] = -n[ii,:].T@K@V[jj,kk,:]*k_loc[jj]
+                        omega[ii,jj,kk] = -n[ii,:].T@K@V[jj,kk,:]
             
             #print(omega)
 
             A = np.array([[omega[0,0,0]+omega[0,1,1],omega[0,1,0]              ,0                          ,omega[0,0,1]               ],
-                        [omega[1,1,1]             ,omega[1,1,0]+omega[1,2,1]  ,omega[1,2,0]              ,0                          ],
+                        [omega[1,1,1]             ,omega[1,1,0]+omega[1,2,1]  ,+omega[1,2,0]              ,0                          ],
                         [0                        ,omega[2,2,1]               ,omega[2,2,0]+omega[2,3,1]  ,omega[2,3,0]              ],
-                        [omega[3,0,0]            ,0                          ,omega[3,3,1]               ,omega[3,3,0]+omega[3,0,1]  ]])
+                        [omega[3,0,0]            ,0                          ,+omega[3,3,1]               ,+omega[3,3,0]+omega[3,0,1]  ]])
             
 
 
@@ -111,16 +115,18 @@ def compute_matrix(mesh,K):
                         [0                        ,0                         ,0                       ,omega[3,3,0]+omega[3,3,1]]])
             T = C@np.linalg.inv(A)@B-D
 
+            T = T
+
             assembler = lambda vec: local_assembler(i,j,vec)
 
-            matrix[meshToVec(i-1,j-1),:] += assembler(T[0,:] - T[3,:])
+            matrix[meshToVec(i-1,j-1),:] += assembler(T[0,:] )
 
-            matrix[meshToVec(i-1,j),:] += assembler(-T[0,:] - T[1,:])
+            matrix[meshToVec(i-1,j),:] += assembler(  -T[1,:])
 
 
-            matrix[meshToVec(i,j),:] += assembler(T[1,:] - T[2,:])
+            matrix[meshToVec(i,j),:] += assembler( T[2,:])
 
-            matrix[meshToVec(i,j-1),:] += assembler(T[2,:] + T[3,:])
+            matrix[meshToVec(i,j-1),:] += assembler( -T[3,:])
     for i in range(cell_centers.shape[0]):
         for j in range(cell_centers.shape[1]):
             if (i==0) or (i==ny-2) or (j==0) or (j==nx-2):
@@ -144,6 +150,7 @@ def compute_vector(mesh,f,boundary):
     h_y = nodes[1,0,1]-nodes[0,0,1]
     for i in range(cell_centers.shape[0]):
         for j in range(cell_centers.shape[1]):
+
             if (i==0) or (i==ny-2) or (j==0) or (j==nx-2):
                 vector[meshToVec(i,j)]= boundary(cell_centers[i,j,0],cell_centers[i,j,1])
                 continue
--- main.py
+++ main.py
@@ -7,22 +7,37 @@ import math
 import matplotlib.pyplot as plt
 
 K = np.array([[1,0],[0,1]])
+transform = np.array([[1,0],[0.1,1]])
 nx = 6
 ny = 6
 x = sym.Symbol('x')
 y = sym.Symbol('y')
 u_fabric = sym.cos(y*math.pi)*sym.cosh(x*math.pi)
-f = -divergence(gradient(u_fabric,[x,y]),[x,y],permability_tensor=K)
-source = sym.lambdify([x,y],f)
 
+
+#u_fabric = (x*y*(1-x)*(1-y))
+source = -divergence(gradient(u_fabric,[x,y]),[x,y],permability_tensor=K)
+print(source)
+source = sym.lambdify([x,y],source)
 u_lam = sym.lambdify([x,y],u_fabric)
 
 
 
-T1 = lambda x,y: (0.9*y+0.1)*math.sqrt(x) + (0.9-0.9*y)*x**2
-T = lambda x,y: x
+#T1 = lambda x,y: (0.9*y+0.1)*math.sqrt(x) + (0.9-0.9*y)*x**2
+T = lambda x,y: x + 0.3*y
 
-    
+mesh = Mesh(4,4,T)
+
+mesh.plot()
+print('noden ',mesh.nodes[1,1])
+print('har cellecenter ',mesh.cell_centers[1,1])
+print('og midpunkter ',mesh.midpoints[1,1,:,:])
+print('og normalvektorer ',mesh.normals[1,1,:,:])
+
+
+
+
+
 def compute_error(mesh,u,u_fabric):
     cx = mesh.cell_centers.shape[1]
     cy = mesh.cell_centers.shape[0]
@@ -33,20 +48,25 @@ def compute_error(mesh,u,u_fabric):
         for j in range(cx):
             u_fabric_vec[mesh.meshToVec(i,j)] = u_fabric(mesh.cell_centers[i,j,0],mesh.cell_centers[i,j,1])
             volumes[mesh.meshToVec(i,j)] = mesh.volumes[i,j]
-
-    return math.sqrt(np.square(u-u_fabric_vec).T@volumes/(np.ones(volumes.shape).T@volumes))
+    mesh.plot_vector(u-u_fabric_vec,'error')
+    err = math.sqrt(np.square(u-u_fabric_vec).T@volumes/(np.ones(volumes.shape).T@volumes))
+    print(err)
+    return err
 
 def run_test(K,source,u_fabric,n,T):
     mesh = Mesh(n,n,T)
+    mesh.plot()
     A = compute_matrix(mesh,K)
     f = compute_vector(mesh,source,u_fabric)
     u = np.linalg.solve(A,f)
+    mesh.plot_vector(u,'computed solution')
+    mesh.plot_funtion(u_fabric,'exact solution')
     return compute_error(mesh,u,u_fabric)
 
-result = np.zeros((4,2))
-for i in range(3,7):
-    result[i-3,0] = math.log(2**i,2)
-    result[i-3,1] = math.log(run_test(K,source,u_lam,2**i,T),2)
+# result = np.zeros((4,2))
+# for i in range(3,7):
+#     result[i-3,0] = math.log(2**i,2)
+#     result[i-3,1] = math.log(run_test(K,source,u_lam,2**i,T),2)
 
 plt.plot(result[:,0],result[:,1],'o-')
 
